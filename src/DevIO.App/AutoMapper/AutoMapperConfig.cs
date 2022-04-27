5c7194c1467811884641243be30af894acc41a2e
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2022-04-27 15:55:23 -0300

    first commit

---
+++ src/DevIO.App/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DevIO.App.ViewModels;
+using DevIO.Business.Models;
+
+namespace DevIO.App.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+            
+        }
+    }
+}
